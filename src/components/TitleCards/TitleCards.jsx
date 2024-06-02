f9579f86b6837fa42ae629b5a52fcadc7f6fc21d
yasai-nodame <yasaisecret1@gmail.com>
2024-06-02 21:49:56 +0900

    カテゴリ追加

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,21 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title, category}) => {
 
+  const[apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWJjYmVlNGJkZjMwOGExODQ0OGYyNmI1MWVmMzUwYyIsInN1YiI6IjY2NWM1ZjE3ZTBlM2IzMWFkN2NjNGViNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i0Lrf_CRPBU0CBKvfA8sCdaaPylN-d5WHPcjqKU-1io'
+    }
+  };
+    
 
   const handleWheel = (event) => {
     event.preventDefault;
@@ -14,16 +24,22 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel);
   }, []);
   return (
     <div className='title-cards'>
       <h2>{title?title:"Netflixで人気の作品"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
+        {apiData.map((card, index)=>{
           return <div className="card" key={index}>
-            <img src={card.image} alt=""/>
-            <p>{card.name}</p>
+            <img src={'https://image.tmdb.org/t/p/w500' + card.backdrop_path} alt=""/>
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
