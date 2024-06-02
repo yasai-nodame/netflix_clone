768a21d93414a2df0751b993f0241dbb2c6fe362
yasai-nodame <yasaisecret1@gmail.com>
2024-06-02 21:51:01 +0900

    useStateによる効率的な変更を追加

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+  const [signState, setSignState] = useState("ログイン")
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt=""/>
       <div className='login-form'>
-        <h1>ログイン</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='お名前'/>
+          {signState==="新規登録"?<input type="text" placeholder='お名前'/>:<></>}
           <input type="email" placeholder='メールアドレス'/>
           <input type="password" placeholder='パスワード'/>
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className='form-help'>
             <div className='remember'>
               <input type="checkbox"/>
@@ -22,8 +25,10 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch">
-          <p>初めてご利用ですか？ <span>新規登録はこちら</span></p>
-          <p>アカウントをお持ちの方 <span>ログインする</span></p>
+          {signState==="ログイン"?
+          <p>初めてご利用ですか？ <span onClick={() => {setSignState("新規登録")}}>新規登録はこちら</span></p>
+          :<p>アカウントをお持ちの方 <span onClick={() => {setSignState("ログイン")}}>ログインする</span></p>
+        }
         </div>
       </div>
     </div>
