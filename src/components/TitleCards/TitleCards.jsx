41a90380a45358a491c41fa2d327e6a5a261633a
yasai-nodame <yasaisecret1@gmail.com>
2024-06-03 23:14:34 +0900

    映画に関連付けられたIDを付与

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom'
+
 
 
 const TitleCards = ({title, category}) => {
@@ -37,10 +39,10 @@ const TitleCards = ({title, category}) => {
       <h2>{title?title:"Netflixで人気の作品"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index)=>{
-          return <div className="card" key={index}>
+          return <Link to={`/player/${card.id}`}className="card" key={index}>
             <img src={'https://image.tmdb.org/t/p/w500' + card.backdrop_path} alt=""/>
             <p>{card.original_title}</p>
-          </div>
+          </Link>
         })}
       </div>
     </div>
