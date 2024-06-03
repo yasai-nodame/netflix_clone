b956411bf50f7d844fd88cf800f6421ef828152a
yasai-nodame <yasaisecret1@gmail.com>
2024-06-03 23:16:59 +0900

    映画に関連付けられたkeyを取得＆fetchに映画に関連付けられたIDの情報を取得

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YWJjYmVlNGJkZjMwOGExODQ0OGYyNmI1MWVmMzUwYyIsInN1YiI6IjY2NWM1ZjE3ZTBlM2IzMWFkN2NjNGViNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.i0Lrf_CRPBU0CBKvfA8sCdaaPylN-d5WHPcjqKU-1io'
+    }
+  };
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  }, [])
+  
+  
+
+
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt=""  onClick={()=>{navigate(-1)}}/>
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
