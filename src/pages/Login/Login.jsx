48eff63035545488c58a992060fe075639e78296
yasai-nodame <yasaisecret1@gmail.com>
2024-06-04 23:05:21 +0900

    firebase追加

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
+import { login, signup } from '../../firebase'
 
 const Login = () => {
 
   const [signState, setSignState] = useState("ログイン")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const user_auth = async(event)=>{
+    event.preventDefault();
+    if (signState==="ログイン") {
+      await login(email, password);
+    } else {
+      await signup(name, email, password);
+    }
+  }
 
   return (
     <div className='login'>
@@ -12,10 +25,10 @@ const Login = () => {
       <div className='login-form'>
         <h1>{signState}</h1>
         <form>
-          {signState==="新規登録"?<input type="text" placeholder='お名前'/>:<></>}
-          <input type="email" placeholder='メールアドレス'/>
-          <input type="password" placeholder='パスワード'/>
-          <button>{signState}</button>
+          {signState==="新規登録"?<input value={name} onChange={(e)=>{setName(e.target.value)}} type="text" placeholder='お名前'/>:<></>}
+          <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder='メールアドレス'/>
+          <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='パスワード'/>
+          <button onClick={user_auth} type="submit">{signState}</button>
           <div className='form-help'>
             <div className='remember'>
               <input type="checkbox"/>
